8ec6bb89405dd2aafb1cca459d48b36b83dd5a3a
hamedjonas <jonas.hamed@twinformatics.at>
2026-09-14 16:35:58 +0200

    changed presets

--- server/data/presets.js
+++ server/data/presets.js
@@ -1,27 +1,39 @@
 // server/config/gamePresets.js
 module.exports = {
   balanced: {
-    revealIntervals: [20, 20, 10, 10, 10, 5, 5],
+    revealIntervals: [5, 10, 10, 10, 10, 10, 10, 5, 5, 5, 2, 2, 2, 2, 2],
     headStartMinutes: 15,
-    roundDurationMinutes: 60,
+    roundDurationMinutes: 90,
     objectiveCount: 4
   },
   hunterFavored: {
-    revealIntervals: [10, 10, 5, 5, 5, 5, 2, 2, 2, 2],
+    revealIntervals: [5, 10, 10, 10, 10, 10, 5, 2, 2, 2, 2, 2],
     headStartMinutes: 15,
-    roundDurationMinutes: 45,
-    objectiveCount: 3
+    roundDurationMinutes: 70,
+    objectiveCount: 4
   },
   runnerFavored: {
-    revealIntervals: [30, 20, 20, 10, 10, 5],
-    headStartMinutes: 20,
-    roundDurationMinutes: 75,
-    objectiveCount: 5
+    revealIntervals: [5, 15, 10, 10, 10, 10, 10, 5, 5, 5, 5, 2, 2, 2, 2, 2],
+    headStartMinutes: 15,
+    roundDurationMinutes: 100,
+    objectiveCount: 4
+  },
+  quickGame: {
+    revealIntervals: [5, 10, 10, 10, 5, 5, 2, 2, 1],
+    headStartMinutes: 10,
+    roundDurationMinutes: 50,
+    objectiveCount: 3
+  },
+  superMegaCrazyLongGame: {
+    revealIntervals: [5, 15, 10, 10, 10, 10, 5, 5, 5, 5, 5, 5, 15, 15, 10, 10, 10, 10, 5, 5, 2, 2, 2, 2, 2],
+    headStartMinutes: 15,
+    roundDurationMinutes: 180,
+    objectiveCount: 9
   },
   debug: {
     revealIntervals: [0.1, 0.1, 0.1, 1],
     headStartMinutes: 0.2,
     roundDurationMinutes: 5,
-    objectiveCount: 2
+    objectiveCount: 3
   }
 };
